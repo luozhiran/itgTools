c525d025adaff0f9f4b71f55051c75831f6af235
luozhiran <zhiran.luo@51job.com>
2026-07-01 09:49:57 +0800

    增加基础功能库

--- itg-base/src/main/java/com/itg/itg_base/AutoBindingBaseFragment.kt
+++ itg-base/src/main/java/com/itg/itg_base/AutoBindingBaseFragment.kt
@@ -7,12 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
-import com.example.itg_base.ability.MessageAbility
-import com.example.itg_base.ability.PermissionAbility
-import com.example.itg_base.ability.UiStateAbility
-import com.example.itg_base.ability.ViewBindingAbility
-import com.example.itg_base.ability.ViewModelAbility
 import com.example.itg_base.arch.ItgModel
+import com.itg.itg_base.ability.MessageAbility
+import com.itg.itg_base.ability.PermissionAbility
+import com.itg.itg_base.ability.UiStateAbility
+import com.itg.itg_base.ability.ViewBindingAbility
+import com.itg.itg_base.ability.ViewModelAbility
 import java.lang.reflect.ParameterizedType
 
 /**
@@ -115,6 +115,7 @@ abstract class AutoBindingBaseFragment<
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+        viewModelStatesObserved = false  // 视图重建时允许重新注册观察者
     }
 
     /**
