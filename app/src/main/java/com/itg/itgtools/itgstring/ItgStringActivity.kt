b6853c94e41e0f688bd6413bb6eb7e9157fc9ccd
android <android@test.com>
2026-07-04 17:23:05 +0800

    升级jvm

---
+++ app/src/main/java/com/itg/itgtools/itgstring/ItgStringActivity.kt
@@ -0,0 +1,18 @@
+package com.itg.itgtools.itgstring
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.itg.itg_base.AutoBindingBaseActivity
+import com.itg.itgtools.R
+import com.itg.itgtools.databinding.ActivityItgStringBinding
+import com.therouter.router.Route
+
+@Route(path = "itg/itgStringActivity")
+class ItgStringActivity : AutoBindingBaseActivity<ActivityItgStringBinding, ItgStringModel>() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/itg/itgtools/itgstring/ItgStringModel.kt
@@ -0,0 +1,7 @@
+package com.itg.itgtools.itgstring
+
+import android.app.Application
+import com.example.itg_base.arch.ItgModel
+
+class ItgStringModel(app: Application): ItgModel(app) {
+}
