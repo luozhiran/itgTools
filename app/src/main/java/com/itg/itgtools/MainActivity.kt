b95b52f6f4ced86f0557c89c6aecf987bde9b3c9
luozhiran <zhiran.luo@51job.com>
2026-07-14 09:38:40 +0800

    引入webview

--- app/src/main/java/com/itg/itgtools/MainActivity.kt
+++ app/src/main/java/com/itg/itgtools/MainActivity.kt
@@ -6,6 +6,7 @@ import com.itg.itg_base.AutoBindingBaseActivity
 import com.itg.itgtools.pages.itgui.ItgUiExamplesActivity
 import com.itg.itgtools.databinding.ActivityMainBinding
 import com.itg.itgtools.route.RoutePath
+import com.itg.itgtools.webcache.WebCacheDemoActivity
 import com.therouter.router.Route
 
 @Route(path = RoutePath.MAIN_HOME_ACTIVITY)
@@ -14,15 +15,10 @@ class MainActivity : AutoBindingBaseActivity<ActivityMainBinding, MainModel>() {
         super.onCreate(savedInstanceState)
         binding.openItgUiExamples.setOnClickListener {
             startActivity(Intent(this, ItgUiExamplesActivity::class.java))
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-        findViewById<android.view.View>(R.id.openWebCacheDemoButton).setOnClickListener {
-            startActivity(Intent(this, WebCacheDemoActivity::class.java))
+
+            findViewById<android.view.View>(R.id.openWebCacheDemoButton).setOnClickListener {
+                startActivity(Intent(this, WebCacheDemoActivity::class.java))
+            }
         }
     }
 }
